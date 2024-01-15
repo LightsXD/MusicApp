7d45f8b973418bd974a9e109ba37579d209a6650
LightsXD <pranavpatil4159@outlook.com>
2024-01-15 17:09:50 +0530

    HorizontalScrolling

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import React from "react";
 import './App.css';
 import { Navbar} from './Navbar';
 import { HorizontalScroll} from './HorizontalScroll';
+import {data} from "./mockData"
 function App() {
+
   return (
     <>
     <Navbar/>
-    <HorizontalScroll/>
+    <HorizontalScroll songs = {data} />
+    <HorizontalScroll songs = {data} NoImg={true}/>
     </>
   );
 }
--- src/HorizontalScroll.js
+++ src/HorizontalScroll.js
@@ -1,32 +1,42 @@
 import React from "react";
-import {data} from "./mockData"
+import { useEffect, useState } from "react"
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { v4 as uuidv4 } from 'uuid';
 
-export function HorizontalScroll() {
+export function HorizontalScroll({ songs, NoImg }) {
+  const [uniqueId, setUniqueId] = useState('');
+
+    useEffect(() => {
+      // Generate a unique ID using uuidv4() when the component mounts
+      const id = uuidv4();
+      setUniqueId(id);
+    }, []); // Empty dependency array ensures the effect runs only once
+
+    const ScrollBar_id= crypto.randomUUID.toString();
     const slideLeft = () => {
-      var slider = document.getElementById('slider');
+      var slider = document.getElementById(uniqueId);
       slider.scrollLeft = slider.scrollLeft - 500;
     };
   
     const slideRight = () => {
-      var slider = document.getElementById('slider');
+      var slider = document.getElementById(uniqueId);
       slider.scrollLeft = slider.scrollLeft + 500;
     };
-  
     return (
       <>
         <img
           className='w-full h-[440px] object-cover'
           src='https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2607&q=80'
           alt=''
+          style={{ display: NoImg ? 'none' : 'block' }}
         />
         <div className='relative flex items-center'>
           <MdChevronLeft className='opacity-50 cursor-pointer hover:opacity-100' onClick={slideLeft} size={40} />
           <div
-            id='slider'
+            id={uniqueId}
             className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide'
           >
-            {data.map((item) => (
+            {songs.map(item => (
               <img
                 className='w-[220px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300'
                 src={item.img}
